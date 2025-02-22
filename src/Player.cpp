6a6ab3b061d563552565368fb393e0aa12f378d1
Rasmus Aaen <ra@emd.dk>
2025-02-22 15:54:18 +0100

    Optimize msg buffer

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,27 +1,27 @@
 ﻿#include <Arduino.h>
-#include "Config.h"
+#include "config.h"
 #include "I2C.h"
 #include "Player.h"
 
 void Player::play(byte bank, byte track) {
-  char msg[8];
-  snprintf(msg, sizeof(msg), "$%i%i\r", bank, track);
+  char msg[6];
+  snprintf(msg, sizeof(msg), "$%d%02d\r", bank, track);
   i2c::sendMessage(I2C_MP3, msg);
 }
 
 void Player::playRandom(byte bank, byte trackMin, byte trackMax) {
   // play a random sound
-  char msg[8];
-  snprintf(msg, sizeof(msg), "$%i%i\r", bank, (int)random(trackMin,trackMax+1));
+  char msg[6];
+  snprintf(msg, sizeof(msg), "$%d%02d\r", bank, (int)random(trackMin,trackMax+1));
   i2c::sendMessage(I2C_MP3, msg);
 }
 
 void Player::volumeUp() {
-  char msg[8] = "$V+\r";
+  char msg[5] = "$V+\r";
   i2c::sendMessage(I2C_MP3, msg);
 }
 
 void Player::volumeDown() {
-  char msg[8] = "$V-\r";
+  char msg[5] = "$V-\r";
   i2c::sendMessage(I2C_MP3, msg);
 }
--- src/Teeces.cpp
+++ src/Teeces.cpp
@@ -1,63 +1,63 @@
 ﻿#include "Teeces.h"
-#include "Config.h"
+#include "config.h"
 #include "I2C.h"
 
 #include <Arduino.h>
 
 void Teeces::test(displayid display)
 {
-  char msg[8];
-  snprintf(msg, sizeof(msg), "@%iT0\r", display);
+  char msg[6];
+  snprintf(msg, sizeof(msg), "@%dT0\r", display);
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
 void Teeces::off(displayid display)
 {
-  char msg[8];
-  snprintf(msg, sizeof(msg), "@%iT20\r", display);
+  char msg[7];
+  snprintf(msg, sizeof(msg), "@%dT20\r", display);
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
 void Teeces::random(displayid display)
 {
-  char msg[8];
-  snprintf(msg, sizeof(msg), "@%iT1\r", display);
+  char msg[6];
+  snprintf(msg, sizeof(msg), "@%dT1\r", display);
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
 void Teeces::alarm()
 {
-  char msg[8] = "@0T3\r";
+  char msg[6] = "@0T3\r";
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
 void Teeces::shortCircuit()
 {
-  char msg[8] = "@0T4\r";
+  char msg[6] = "@0T4\r";
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
 void Teeces::scream()
 {
-  char msg[8] = "@0T5\r";
+  char msg[6] = "@0T5\r";
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
 void Teeces::leia()
 {
-  char msg[8] = "@0T6\r";
+  char msg[6] = "@0T6\r";
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
 void Teeces::starWars()
 {
-  char msg[8] = "@0T10\r";
+  char msg[7] = "@0T10\r";
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
 void Teeces::march()
 {
-  char msg[8] = "@0T11\r";
+  char msg[7] = "@0T11\r";
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
@@ -66,8 +66,8 @@ void Teeces::bargraph(displayid display)
   if((display < 0) || (display > 3)) {
     return;
   }
-  char msg[8];
-  snprintf(msg, sizeof(msg), "@%iT92\r", display);
+  char msg[7];
+  snprintf(msg, sizeof(msg), "@%dT92\r", display);
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
@@ -76,19 +76,19 @@ void Teeces::displayText(displayid display, char *text)
   if((display < 0) || (display > 3)) {
     return;
   }
-  char msg[sizeof(text)];
+  char msg[sizeof(text)+5];
 
-  snprintf(msg, sizeof(msg), "@%iM%s\r", display, text);
+  snprintf(msg, sizeof(msg), "@%dM%s\r", display, text);
   i2c::sendMessage(I2C_TEECES, msg);
 
-  snprintf(msg, sizeof(msg), "@%iT100\r", display);
+  snprintf(msg, sizeof(msg), "@%dT100\r", display);
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
 void Teeces::setLanguage(displayid display, language language)
 {
-  char msg[8];
-  snprintf(msg, sizeof(msg), "@%iT%d\r", display, language);
+  char msg[6];
+  snprintf(msg, sizeof(msg), "@%dT%d\r", display, language);
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
@@ -97,8 +97,8 @@ void Teeces::setRandomApperance(displayid display, byte type)
   if((display < 0) || (display > 3)) {
     return;
   }
-  char msg[8];
-  snprintf(msg, sizeof(msg), "@%iR%d\r", display, type);
+  char msg[6];
+  snprintf(msg, sizeof(msg), "@%dR%d\r", display, type);
   i2c::sendMessage(I2C_TEECES, msg);
 }
 
@@ -107,7 +107,7 @@ void Teeces::setPSIstate(displayid display, psistate state)
   if((display != 0) && (display != 4) && (display != 5)) {
     return;
   }
-  char msg[8];
-  snprintf(msg, sizeof(msg), "@%iS%d\r", display, state);
+  char msg[6];
+  snprintf(msg, sizeof(msg), "@%dS%d\r", display, state);
   i2c::sendMessage(I2C_TEECES, msg);
 }
